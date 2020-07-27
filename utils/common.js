10431df35eaf989e5f4ba3a03fd486c05d2605e3
朱鹏程 <pengcheng.zpc@120yibao.com>
2020-07-27 19:21:09 +0800

    feature: add pack analyse

--- utils/common.js
+++ utils/common.js
@@ -1,4 +1,6 @@
 import execa from 'execa'
+import fse from 'fs-extra'
+import path from 'path'
 
 export async function execOut (...args) {
   const { stdout, stderr, failed } = await execa(...args)
@@ -6,4 +8,23 @@ export async function execOut (...args) {
     throw new Error(stderr)
   }
   return stdout
+}
+
+/**
+ * 递归遍历目录下所有文件
+ * @param dirPath 目录名
+ * @param result
+ */
+export function findAllFiles (dirPath, result = []) {
+  dirPath = dirPath[dirPath.length - 1] === '/' ? dirPath : (dirPath + '/')
+  const fileList = fse.readdirSync(dirPath)
+  fileList.filter(i => !/^\./.test(i)).forEach(i => {
+    const p = dirPath + i
+    const stat = fse.statSync(p)
+    if (stat.isFile()) {
+      result.push(p)
+    } else if (stat.isDirectory()) {
+      findAllFiles(p, result)
+    }
+  })
 }
